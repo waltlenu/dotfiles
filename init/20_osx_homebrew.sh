ca74792ccc56a23e15f7a29160a9de99076bcc06
Walter Lenuzza <walter@lenuzza.eu>
2016-05-20 12:55:38 +0100

    Operator precendence and verbosity

--- init/20_osx_homebrew.sh
+++ init/20_osx_homebrew.sh
@@ -6,22 +6,32 @@ is_osx || return 1
 
 # Install/Update Homebrew.
 if [[ ! "$(command -v brew)" ]]; then
-  e_header "Installing Homebrew"
-  true | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  e_header "Install Homebrew"
+  true | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+    || { e_error "Homebrew install failed" && return 1; }
 else
-  e_header "Updating Homebrew"
-  brew update || e_error "Homebrew update failed" && return 1
+  e_header "Update Homebrew"
+  brew update || { e_error "Homebrew update failed" && return 1; }
 fi
 
+# Tap Homebrew bundle to enable Brewfiles
 e_header "Tap Homebrew bundle to enable Brewfiles"
-brew tap homebrew/bundle || e_error "Homebrew bundle tap failed" && return 1
+brew tap homebrew/bundle || { e_error "Homebrew bundle tap failed" && return 1; }
 
+# Upgrade all brews
+e_header 'Homebrew upgrade all'
+brew upgrade || { e_error "Homebrew upgrade failed" && return 1; }
+
+# Install from Brewfile
 e_header "Homebrew bundle install"
-brew bundle || e_error "Homebrew bundle install failed" && return 1
+brew bundle || { e_error "Homebrew bundle install failed" && return 1; }
 
+# Cleanup all oudated brews
 e_header "Homebrew cleanup any outdated formulas from the cellar"
-brew cleanup || e_error "Homebrew cleanup failed." && return 1
-brew cask cleanup || e_error "Homebrew cask cleanup failed." && return 1
+brew cleanup || { e_error "Homebrew cleanup failed" && return 1; }
+brew cask cleanup || { e_error "Homebrew cask cleanup failed" && return 1; }
+echo; e_success "Homebrew up-to-date"; echo
 
 # Check Homebrew's status
-brew doctor || e_error "Homebrew doctor failed." && return 1
+brew doctor || { echo; e_error "Homebrew doctor failed" && return 1; }
+
