2ef69bbfad7fadc6d27301efbfbea252fe2738ae
Walter Lenuzza <walter@lenuzza.eu>
2016-05-20 12:10:42 +0100

    Verbosity improved

--- init/10_osx_updates.sh
+++ init/10_osx_updates.sh
@@ -4,15 +4,18 @@
 # OSX-only stuff. Abort if not OSX.
 is_osx || return 1
 
-# Ask for the administrator password upfront
+# Ask for the administrator password upfront (and extend timeout by 5 mins)
+e_header "Ask for the administrator upfront"
 sudo -v
 
 # MacOSX App Store updates
 e_header "MacOSX App Store updates"
-sudo softwareupdate -i -a
+sudo softwareupdate -i -a && e_success 'MacOSX up-to-date'
 
-# Install XCode Commandline Tools.
-e_header "Installing XCode Commandline Tools"
+# Install XCode Commandline Tools
+e_header "XCode Commandline Tools"
 if [[ ! "$(xcode-select -p)" ]]; then
-  xcode-select --install
+  xcode-select --install && e_success 'XCode Commandline Tools installed'
+else
+  e_success 'XCode Commandline Tools already installed'
 fi
