bf217d6939ca51beb1b0a6587c423830af49e4cf
Walter Lenuzza <waltlenu@users.noreply.github.com>
2023-09-08 09:00:14 +0100

    fix: deprecate exa for eza

--- home/private_dot_config/shell/03_aliases.sh
+++ home/private_dot_config/shell/03_aliases.sh
@@ -16,26 +16,26 @@ alias egrep='egrep --color=auto'
 #
 ## List files
 #
-if [ "$(command -v exa)" ]; then
-  alias v='exa -a --git --long --header --time-style=long-iso'                     # Long listing
-  alias vs='exa -a --git --long --header --time-style=long-iso --sort=size'        # Sort by size
-  alias vt='exa -a --git --long --header --time-style=long-iso --sort=oldest'      # Sort by mtime-reversed
-  alias vx='exa -a --git --long --header --time-style=long-iso --group --extended' # Show ACLs and xattrs
-  alias tree='exa -a --ignore-glob=".git" --tree'
+if [ "$(command -v eza)" ]; then
+	alias v='eza -a --git --long --header --time-style=long-iso'                     # Long listing
+	alias vs='eza -a --git --long --header --time-style=long-iso --sort=size'        # Sort by size
+	alias vt='eza -a --git --long --header --time-style=long-iso --sort=oldest'      # Sort by mtime-reversed
+	alias vx='eza -a --git --long --header --time-style=long-iso --group --extended' # Show ACLs and xattrs
+	alias tree='eza -a --ignore-glob=".git" --tree'
 else
-  alias la='ls -A --color=auto'
-  alias v='ls -lhA'    # Long listing
-  alias vs='ls -lhASr' # Sort by size-reversed
-  alias vt='ls -lhAtr' # Sort by mtime-reversed
-  alias vx='ls -lhAe@' # Show ACLs and xattrs
-  alias tree='tree -aC -I .git --dirsfirst'
+	alias la='ls -A --color=auto'
+	alias v='ls -lhA'    # Long listing
+	alias vs='ls -lhASr' # Sort by size-reversed
+	alias vt='ls -lhAtr' # Sort by mtime-reversed
+	alias vx='ls -lhAe@' # Show ACLs and xattrs
+	alias tree='tree -aC -I .git --dirsfirst'
 fi
 
 # Disk usage
 if [ "$(command -v ncdu)" ]; then
-  alias d='ncdu'
+	alias d='ncdu'
 else
-  alias d='du -csh | sort -nk'
+	alias d='du -csh | sort -nk'
 fi
 
 # Aliases: misc
@@ -56,24 +56,24 @@ alias dotfiles='chezmoi'
 
 # Default programs
 if [ "$(command -v most)" ]; then
-  export PAGER='most'
-  alias less='most'
+	export PAGER='most'
+	alias less='most'
 else
-  export PAGER='less'
-  export LESS='-FMnqrX'
+	export PAGER='less'
+	export LESS='-FMnqrX'
 fi
 
 # Editor
 [ "$(command -v vim)" ] && {
-  alias vi='vim'
-  export EDITOR='vim'
-  export VISUAL='vim'
+	alias vi='vim'
+	export EDITOR='vim'
+	export VISUAL='vim'
 }
 
 [ "$(command -v nvim)" ] && {
-  alias vi='nvim'
-  export EDITOR='nvim'
-  export VISUAL='nvim'
+	alias vi='nvim'
+	export EDITOR='nvim'
+	export VISUAL='nvim'
 }
 
 # Kubernetes
